e9282ccd3513803e9043454485595d241fb8d03a
nikiitasharrma <nikita16301@gmail.com>
2024-02-13 10:14:53 +0530

    Sent Model from Controller to View

--- Configuration/src/main/java/com/spring/mvc/controllers/HomeController.java
+++ Configuration/src/main/java/com/spring/mvc/controllers/HomeController.java
@@ -1,14 +1,28 @@
 package com.spring.mvc.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {      
 	
 	@RequestMapping("/home")
-	public String home() {
+	public String home(Model model) {
 		System.out.println("home controller!");
+		
+		model.addAttribute("name", "Nikita Sharma");
+		model.addAttribute("age", 22);
+		
+		List<String> friends = new ArrayList<String>();
+		friends.add("Nandini");
+		friends.add("Urvashi");
+		
+		model.addAttribute("f",friends);
+		
 		return "home";
 	}
 	
--- Configuration/src/main/webapp/WEB-INF/views/home.jsp
+++ Configuration/src/main/webapp/WEB-INF/views/home.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
     pageEncoding="ISO-8859-1"%>
+<%@ page import="java.util.List" %>
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,6 +8,20 @@
 <title>Home</title>
 </head>
 <body>
-   <p>This is home page</p>
+   <p>This is home page
+   
+   <%
+   String name = (String) request.getAttribute("name");
+   Integer age = (Integer) request.getAttribute("age");
+   List<String> friends = (List<String>) request.getAttribute("f");%>
+   
+   <br>My name is <%=name%>
+   <br>My age is <%=age%> </p>
+   <br>My friends are:
+   <%
+   for(String s : friends){
+   %>
+   <br><%=s %>
+   <%} %>
 </body>
 </html>
